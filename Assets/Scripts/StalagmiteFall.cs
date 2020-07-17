c57f2731da7f5fa0448ce4e6eb0da4807b303759
JerryChenDigipen <67754245+JerryChenDigipen@users.noreply.github.com>
2020-07-17 03:06:28 -0700

    the statggaetasb falls and kills but never respawns

--- Assets/Scripts/StalagmiteFall.cs
+++ Assets/Scripts/StalagmiteFall.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class StalagmiteFall : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    Vector2 startPos;
+    GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        startPos = this.transform.position;
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rb.bodyType == RigidbodyType2D.Static){
+            if (Mathf.Abs(Player.transform.position.x - startPos.x) < 5){
+                rb.bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
     }
 }
