fe5322934439319141bfc80d6056623319b6aee7
JerryChenDigipen <67754245+JerryChenDigipen@users.noreply.github.com>
2020-07-17 13:37:04 -0700

    final screen?

---
+++ Assets/Scripts/FinalTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D coll){
+        if (coll.CompareTag("FinalTransition")){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
