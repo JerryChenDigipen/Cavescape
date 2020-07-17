2ff937758107d94f9349b2e10fafd5f8063df48c
JerryChenDigipen <67754245+JerryChenDigipen@users.noreply.github.com>
2020-07-17 00:32:49 -0700

    slimes work (just #1 tho)

---
+++ Assets/Scripts/SlimeMvmt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeMvmt : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+    }
+    void OnTriggerEnter2D(Collider2D coll){
+        if (coll.CompareTag("SlimeStopper")){
+            moveSpeed *= -1;
+        }
+    }
+}
